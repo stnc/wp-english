265615d85965d1c156fa42de21abb08cd685fae9
stnc <samtunc@yahooo.com>
2025-03-16 17:27:27 -0600

    php add

--- submit.php
+++ submit.php
@@ -1,6 +1,59 @@
-<?php 
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(- 1);
+
+/**
+ * STNC FRAMEWORK
+ *
+ * Author(s): Selman TUNÇ www.selmantunc.com <selmantunc@gmail.com>
+ *
+ * Licensed under the MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author Selman TUNÇ <selmantunc@gmail.com>
+ * @copyright Copyright (c) 2017
+ * @link http://github.com/stnc
+ * @version 3.0.0.0
+ * @date 26.03.2017
+ * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+
+// TODO : urunler kategorisinde yan bar daki alana her alanın adetini yazacak
+if (file_exists('vendor/autoload.php')) {
+    require 'vendor/autoload.php';
+} else {
+    echo "<h1>Lütfen composer.json ı yükleyin </h1>";
+    echo "<p>Örnekler <a href='https://getcomposer.org/doc/00-intro.md#globally'>https://getcomposer.org/doc/00-intro.md#globally</a></p>";
+    echo "<p> terminal yada cmd yi açarak  'composer install' yazınız</p>";
+    echo "<p> eğer yuklü ise terminal yada cmd yi açarak  'composer update' yazınız</p>";
+    exit();
+}
+
+// if (! is_readable('vendor/stnc/framework/src/Core/Config.php')) {
+//     die('config.php bulunamadı, config.example.php dosyasının ismini değiştirip config.php yapınız ve  app/core. içine atınız ');
+// }
+
+use Nette\Utils\Arrays;
+
+$arr = [
+    ['name' => 'John', 'age' => 11],
+    ['name' => 'Mary', 'age' => null],
+    // ...
+];
+$result = Arrays::associate($arr, 'name');
+
+
 $field_values_array = $_POST['field_name'];
 
+echo "<pre>";
 print_r($field_values_array);
+print_r($result);
+
+echo json_encode($field_values_array,JSON_FORCE_OBJECT);
+
+
+
 
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'stnc/wpfw',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '19f9459a0daf5feef2288c004bb8335e879c7790',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nette/utils' => array(
+            'pretty_version' => 'v4.0.5',
+            'version' => '4.0.5.0',
+            'reference' => '736c567e257dbe0fcf6ce81b4d6dbe05c6899f96',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nette/utils',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stnc/wpfw' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '19f9459a0daf5feef2288c004bb8335e879c7790',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
